194af7f18446e28572a0a28d5d658af5f5ace0db
eldermiang <61782036+eldermiang@users.noreply.github.com>
2021-04-19 12:45:46 +1000

    Create Dummy Scene
    Update Lights + Meshes

--- a4/build.js
+++ a4/build.js
@@ -1,22 +1,61 @@
 var plane = null;
+var box = null;
+var sun = null;
 
 var ambientLight;
+var hemiLight;
+var dirLight;
 
 function createPlane() {
     var planeMaterial = new THREE.MeshLambertMaterial();
     planeMaterial.color = new THREE.Color(0.7, 0.7, 0.7);
+    //planeMaterial.color = new THREE.Color(0xf9d71c);
     planeMaterial.side = THREE.DoubleSide;
 
     var planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
     plane = new THREE.Mesh(planeGeometry, planeMaterial);
 }
 
+function createBox() {
+    var boxMaterial = new THREE.MeshBasicMaterial();
+    boxMaterial.color = new THREE.Color(0.5, 0.5 , 0.5);
+
+    var boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+    box = new THREE.Mesh(boxGeometry, boxMaterial);
+    box.position.set(0, 0, 0.5);
+}
+
 function createLight() {
-    ambientLight = new THREE.AmbientLight(new THREE.Color(1, 1, 1), 0.5);
+    //ambientLight = new THREE.AmbientLight(new THREE.Color(1, 1, 1), 0.5);
+    hemiLight = new THREE.HemisphereLight(new THREE.Color(1, 1, 1), 1);
+    //hemiLight.groundColor = new THREE.Color(1, 1, 1);
+    hemiLight.skyColor = new THREE.Color(1, 1, 1);
+    hemiLight.position.set(0, 100, 0);
+    //hemiLight.groundColor = new THREE.Color(0, 0, 0);
+}
+
+function createSun() {
+    var sunPosition = new THREE.Vector3(-20, 1, 1);
+
+    var sunMaterial = new THREE.MeshPhongMaterial();
+    sunMaterial.color = new THREE.Color(0xf9d71c);
+
+    var sunGeometry = new THREE.SphereGeometry(1, 32, 32);
+    sun = new THREE.Mesh(sunGeometry, sunMaterial);
+    sun.position.set(sunPosition.x, sunPosition.y, sunPosition.z);
+
+
+    dirLight = new THREE.DirectionalLight(new THREE.Color(0xf9d71c), 1);
+    dirLight.position.set(sunPosition.x, sunPosition.y, sunPosition.z);
+    dirLight.castShadow = true;
 }
 
 function addObjects() {
     scene.add(plane);
+    scene.add(box);
+    scene.add(sun);
     scene.add(camera);
-    scene.add(ambientLight);
+    scene.add(hemiLight);
+    scene.add(dirLight);
+    //scene.add(ambientLight);
 }
--- a4/run.js
+++ a4/run.js
@@ -1,6 +1,19 @@
 setScene();
 createLight();
+createSun();
+
 createPlane();
+createBox();
+
 addObjects();
+
 animate();
+
+// console.log(hemiLight.intensity);
+// console.log(hemiLight.groundColor);
+// console.log(hemiLight.position);
+console.log(sun.position);
+console.log(dirLight.position);
+// console.log(plane.position);
+
 window.addEventListener('resize', resize);
