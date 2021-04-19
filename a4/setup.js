18653ebdffb41cce1d4b5f1dc3c7b81987334637
eldermiang <61782036+eldermiang@users.noreply.github.com>
2021-04-19 11:34:50 +1000

    Add setup + Update HTML

---
+++ a4/setup.js
@@ -0,0 +1,29 @@
+var scene;
+var camera;
+var renderer;
+var controls
+
+function setScene() {
+    scene = new THREE.Scene();
+    
+    var ratio = window.innerWidth / window.innerHeight;
+    camera = new THREE.PerspectiveCamera(45, ratio, 0.1, 1000);
+    camera.position.set(0, 5, 5);
+    camera.lookAt(0, 0, 0);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+}
+
+var resize = function() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    renderer.setSize(width, height);
+    
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    renderer.render(scene, camera);
+}
