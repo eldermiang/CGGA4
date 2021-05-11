53ec762de6f981d68e216119b5ec19fde70151da
eldermiang <61782036+eldermiang@users.noreply.github.com>
2021-05-11 11:07:06 +1000

    Revert "Roll back"
    This reverts commit a3624debdcec63143d964bfa5c7c0d9a97d4d86e.

--- Stuff/run.js
+++
@@ -1,8 +0,0 @@
-setScene();
-addLight();
-createFloor();
-addShapes();
-loadModel('Models/BuildingBasic.obj');
-document.addEventListener('mousedown', onDocumentMouseDown, false);
-animate();
-window.addEventListener('resize', resizeScene);
